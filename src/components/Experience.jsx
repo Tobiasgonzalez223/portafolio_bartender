1b3a4b7527c4307c952199e360e5ea242b39606b
Tobías Gonzalez Parra <93341@sistemas.frc.utn.edu.ar>
2026-08-07 20:51:32 -0300

    Actualizaciones

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -4,21 +4,21 @@ const experienceList = [
     place: 'Unplugged, Córdoba | @unp.cerro',
     period: 'Mayo 2023 - Actualidad',
     description: 'Coordinación del funcionamiento de barra durante servicios de alta demanda. Control y reposición de stock. Apertura y cierre de barra.',
-    reference: 'Juan Manuel Ferreyra (Enc. depósito) - Ref. +54 9 3543692588.'
+    reference: 'Juan Manuel Ferreyra (Enc. depósito) - +54 9 3543692588.'
   },
   {
     role: 'Bartender y Encargado de Barra',
     place: 'Bar El Tori, Córdoba | @bar.eltori',
     period: 'Febrero - Septiembre 2025',
     description: 'Organización y control del funcionamiento de barra. Coordinación de stock general del bar.',
-    reference: 'Leandro Torres (Dueño) - Ref. +54 9 351559 8887.'
+    reference: 'Leandro Torres (Dueño) - +54 9 351559 8887.'
   },
   {
     role: 'Bartender de soporte',
     place: 'Ocaso Bar, Córdoba | @ocasobar_',
     period: '2025 - 2026',
     description: 'Contratado para eventos de alto volumen. Dominio de carta de coctelería clásica y de autor.',
-    reference: 'Nahuel Rubio (Dueño) - Ref. +54 9 351 2435398'
+    reference: 'Nahuel Rubio (Dueño) - +54 9 351 2435398'
   },
 ]
 
